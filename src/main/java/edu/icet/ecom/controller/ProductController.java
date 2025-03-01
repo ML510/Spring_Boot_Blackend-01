8ce5aec5dbffd4e2cba10fee662abe25d4025a5b
ML510 <mr.mahesh.303@gmail.com>
2025-03-01 21:16:29 +0530

    Dependency Injection

--- src/main/java/edu/icet/ecom/controller/ProductController.java
+++ src/main/java/edu/icet/ecom/controller/ProductController.java
@@ -2,19 +2,22 @@ package edu.icet.ecom.controller;
 
 import edu.icet.ecom.model.Product;
 import edu.icet.ecom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController //---> @Controller + @ResponseBody + @Component
 public class ProductController {
+
+    @Autowired
+    ProductService service;
 
     @GetMapping("/products")
     List<Product> getProduct(){
-        ProductService service = new ProductService();
-        List<Product> products = service.getProducts();
 
+        List<Product> products = service.getProducts();
 
         return products;
     }
--- src/main/java/edu/icet/ecom/service/ProductService.java
+++ src/main/java/edu/icet/ecom/service/ProductService.java
@@ -1,10 +1,13 @@
 package edu.icet.ecom.service;
 
 import edu.icet.ecom.model.Product;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class ProductService {
 
     public List<Product> getProducts(){
