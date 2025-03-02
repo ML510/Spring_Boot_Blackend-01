fd5121ee02348257859795ec5ce0a0c7c7b8337b
ML510 <mr.mahesh.303@gmail.com>
2025-03-03 02:18:50 +0530

    Constructor Dependency Injection

--- src/main/java/edu/icet/ecom/controller/ProductController.java
+++ src/main/java/edu/icet/ecom/controller/ProductController.java
@@ -2,23 +2,30 @@ package edu.icet.ecom.controller;
 
 import edu.icet.ecom.model.Product;
 import edu.icet.ecom.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController //---> @Controller + @ResponseBody + @Component
+@RequiredArgsConstructor
 public class ProductController {
 
-    @Autowired
-    ProductService service;
+    // ************* Autowired ***********
+//    @Autowired
+//    ProductService service
+
+    // ********** Constructor Dependency Injection ********
+    final ProductService service;
+
+//    public ProductController(ProductService service) {
+//        this.service = service;
+//    }
 
     @GetMapping("/products")
-    List<Product> getProduct(){
+    List<Product> getProduct() {
 
-        List<Product> products = service.getProducts();
-
-        return products;
+        return service.getProducts();
     }
 }
